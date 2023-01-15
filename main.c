2d96afae69b3c2212b270bc1354da44cb28e1dd4
Brahim Elkartouch <belkarto@e1r13p10.1337.ma>
2023-01-15 17:49:29 +0100

    creat function that get commands <get_cmd>

--- main.c
+++ main.c
@@ -9,27 +9,43 @@
 /*   Updated: 2023/01/11 12:40:10 by belkarto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "my_lib/libft.h"
 #include "pipex.h"
+#include <sys/fcntl.h>
 #include <unistd.h>
 
-int	path_checker(char *cmd)
+//function that check if the cmd exist and initialize path_checker
+/* int	ft_check_cmd(char *cmd, char **env, char *path)
 {
-	return (0);
+} */
+
+char	**fill_cmd(int len, char **av)
+{
+	int		i;
+	char	**holder;
+	char	**cmd;
+
+	cmd = (char **)malloc(sizeof(char *) * len + 1);
+	i = -1;
+	while(++i < len)
+	{
+		holder = ft_split(av[i], ' ');
+		cmd[i] = ft_strdup(holder[0]);
+		ft_free(holder);
+	}
+	cmd[i] = NULL;
+	return (cmd);
 }
 
-int	ft_check_cmd(char *cmd, char **env, t_pip *pip)
+void	get_cmd(t_pip *pip, int ac, char **av, char **env)
 {
-	char	**args;
+	int	i;
 
-	args = ft_split(cmd, ' ');
-	cmd = args[0];
-	if (access(cmd, X_OK) == 0)
-		return (1);
-	else if (path_checker(cmd) == 0)
-		return (2);
-	else
-		ft_printf("ERROR : (%s)\n\t(%s) %s", __FILE__, cmd, strerror(errno));
-	return (0);
+	i = -1;
+	pip->cmd = fill_cmd(ac - 3, av + 2);
+	ft_printf("%s", pip->cmd[2]);
+	(void)av;
+	(void)env;
 }
 
 int	main(int argc, char *argv[], char **envp)
@@ -42,7 +58,13 @@ int	main(int argc, char *argv[], char **envp)
 		ft_printf("\tUSAGE\t: %s infile cmd1 cmd2 outfile\n", argv[0]);
 		return (2);
 	}
-	pip.fd_infile = check(open(argv[1], 2), __FILE__, __LINE__);
-	pip.env_holder = ft_check_cmd(argv[2], envp, &pip);
+	/* if (argc > 5)
+	{
+		ft_printf("too many argument :\n");
+		ft_printf("\tUSAGE\t: %s infile cmd1 cmd2 outfile\n", argv[0]);
+		return (2);
+	} */
+	pip.fd_infile = check(open(argv[1], O_RDONLY), __FILE__, __LINE__);
+	get_cmd(&pip, argc, argv, envp);
 	return (0);
 }
