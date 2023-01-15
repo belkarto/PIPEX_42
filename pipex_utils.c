9143d80753b6b7d1d95bf2ba16448a4f91dfb531
Brahim Elkartouch <belkarto@e1r13p10.1337.ma>
2023-01-15 17:48:01 +0100

    add ft_free to free 2D arr

--- pipex_utils.c
+++ pipex_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: belkarto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 01:22:57 by belkarto          #+#    #+#             */
+/*   Updated: 2023/01/15 01:22:57 by belkarto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex.h"
 
 int	check(int x, char *file, int line)
@@ -8,4 +20,14 @@ int	check(int x, char *file, int line)
 		exit (1);
 	}
 	return (x);
+}
+
+void	ft_free(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		free(str[i++]);
+	free(str);
 }
